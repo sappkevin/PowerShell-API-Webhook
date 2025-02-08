b5ea5c5ab776a3e59ebd45d906b69b8a47df417e
sappkevin <sapp85@gmail.com>
2025-02-07 21:38:05 -0600

    Removed the required modifier from Data

--- src/Models/DtoScript.cs
+++ src/Models/DtoScript.cs
@@ -7,8 +7,8 @@ namespace Webhookshell.Models
     {
         public string ScriptPath { get; set; } = string.Empty;
         public string? Parameters { get; set; }
-        public string Script { get; set; } = string.Empty;  // Added this property
-        public string Key { get; set; } = string.Empty;     // Added this property
+        public string Script { get; set; } = string.Empty;
+        public string Key { get; set; } = string.Empty;
     }
 
     public class DtoResult
@@ -23,7 +23,7 @@ namespace Webhookshell.Models
     public class Result<T>
     {
         public bool Success { get; set; }
-        public required T Data { get; set; }  // Added 'required' modifier
+        public T? Data { get; set; }  // Changed to nullable
         public List<string> Errors { get; set; } = new();
         public bool IsValid => Success && Data != null && Errors.Count == 0;
         public bool IsNotValid => !IsValid;
